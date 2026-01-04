bd1fb4ce734481228b56f7645167d9f780e83bc1
nicoalbanese <gcalbanese96@gmail.com>
2026-01-04 19:12:08 +0100

    Remove memory tool exports

--- src/agent/tools/index.ts
+++ src/agent/tools/index.ts
@@ -5,5 +5,3 @@ export { grepTool } from "./file-system/grep";
 export { globTool } from "./file-system/glob";
 export { bashTool } from "./file-system/bash";
 export { taskTool } from "./task-delegation/task";
-export { memorySaveTool } from "./memory/save";
-export { memoryRecallTool } from "./memory/recall";
