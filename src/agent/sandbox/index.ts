0b79fc2e928120038992b65d23bd432a544814d7
nicoalbanese <gcalbanese96@gmail.com>
2026-01-04 16:52:18 +0100

    Export VercelSandbox from sandbox module

--- src/agent/sandbox/index.ts
+++ src/agent/sandbox/index.ts
@@ -1,2 +1,7 @@
 export type { Sandbox, SandboxStats, ExecResult } from "./interface";
 export { LocalSandbox, createLocalSandbox } from "./local";
+export {
+  VercelSandbox,
+  createVercelSandbox,
+  type VercelSandboxConfig,
+} from "./vercel";
